9bd0d0ce2cb7eeddbe43e41a2dc4eae7488596ef
Eva Kiyomi <eva.furenaide@gmail.com>
2025-10-04 00:07:06 +0300

    add sources

---
+++ dwmstatus.c
@@ -0,0 +1,246 @@
+/*
+ * Copy me if you can.
+ * by 20h
+ */
+
+#define _BSD_SOURCE
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <strings.h>
+#include <sys/time.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#include <ctype.h>
+
+#include <X11/Xlib.h>
+
+char *tzargentina = "America/Buenos_Aires";
+char *tzutc = "UTC";
+char *tzberlin = "Europe/Berlin";
+char *timeZoneKyiv = "Europe/Kyiv";
+
+static Display *dpy;
+
+char *
+smprintf(char *fmt, ...)
+{
+	va_list fmtargs;
+	char *ret;
+	int len;
+
+	va_start(fmtargs, fmt);
+	len = vsnprintf(NULL, 0, fmt, fmtargs);
+	va_end(fmtargs);
+
+	ret = malloc(++len);
+	if (ret == NULL) {
+		perror("malloc");
+		exit(1);
+	}
+
+	va_start(fmtargs, fmt);
+	vsnprintf(ret, len, fmt, fmtargs);
+	va_end(fmtargs);
+
+	return ret;
+}
+
+void
+settz(char *tzname)
+{
+	setenv("TZ", tzname, 1);
+}
+
+char *
+mktimes(char *fmt, char *tzname)
+{
+	char buf[129];
+	time_t tim;
+	struct tm *timtm;
+
+	settz(tzname);
+	tim = time(NULL);
+	timtm = localtime(&tim);
+	if (timtm == NULL)
+		return smprintf("");
+
+	if (!strftime(buf, sizeof(buf)-1, fmt, timtm)) {
+		fprintf(stderr, "strftime == 0\n");
+		return smprintf("");
+	}
+
+	return smprintf("%s", buf);
+}
+
+void
+setstatus(char *str)
+{
+	XStoreName(dpy, DefaultRootWindow(dpy), str);
+	XSync(dpy, False);
+}
+
+char *
+loadavg(void)
+{
+	double avgs[3];
+
+	if (getloadavg(avgs, 3) < 0)
+		return smprintf("");
+
+	return smprintf("%.2f %.2f %.2f", avgs[0], avgs[1], avgs[2]);
+}
+
+char *
+readfile(char *base, char *file)
+{
+	char *path, line[513];
+	FILE *fd;
+
+	memset(line, 0, sizeof(line));
+
+	path = smprintf("%s/%s", base, file);
+	fd = fopen(path, "r");
+	free(path);
+	if (fd == NULL)
+		return NULL;
+
+	if (fgets(line, sizeof(line)-1, fd) == NULL) {
+		fclose(fd);
+		return NULL;
+	}
+	fclose(fd);
+
+	return smprintf("%s", line);
+}
+
+char *
+getbattery(char *base)
+{
+	char *co;
+
+	co = readfile(base, "capacity");
+	
+	if(co == NULL)
+	{
+		free(co);
+		return smprintf("");	
+	}
+	
+	int i = 0;
+    while (i < 4)
+    {	
+        if (!isdigit(co[i]))
+        {
+            co[i] = '\0';
+            break;
+        }
+        i++;
+    }
+
+    char *result = strdup(co);
+    free(co);
+    return result;
+
+}
+
+char *
+gettemperature(char *base, char *sensor)
+{
+	char *co;
+
+	co = readfile(base, sensor);
+	if (co == NULL)
+		return smprintf("");
+	return smprintf("%02.0f°C", atof(co) / 1000);
+}
+
+char *
+execscript(char *cmd)
+{
+	FILE *fp;
+	char retval[1025], *rv;
+
+	memset(retval, 0, sizeof(retval));
+
+	fp = popen(cmd, "r");
+	if (fp == NULL)
+		return smprintf("");
+
+	rv = fgets(retval, sizeof(retval), fp);
+	pclose(fp);
+	if (rv == NULL)
+		return smprintf("");
+    
+	size_t len = strlen(retval);
+	if (len > 0 && retval[len - 1] == '\n') {
+    		retval[len - 1] = '\0';
+	}
+
+	return smprintf("%s", retval);
+}
+
+char* get_battery_dishcharge_time()
+{
+    char* result = NULL;
+    if(strcmp(execscript("cat /sys/class/power_supply/BAT1/status"),"Discharging") == 0)                                                        
+    {
+        result = (char*)malloc(30 * sizeof(char));
+        sprintf(result, "(%s)", execscript("upower -i $(upower -e | grep 'battery') | grep 'time to empty' | awk '{print $4, $5}'"));
+    }
+    
+    return result;
+}
+
+int
+main(void)
+{
+	char *myStatus;
+
+	char *avgs;
+	char *bat;
+	char *remain_battery;
+	char *t0;
+	char *kbmap;
+	char *soundVolume;
+	char *kyivTime;
+
+	if (!(dpy = XOpenDisplay(NULL))) {
+		fprintf(stderr, "dwmstatus: cannot open display.\n");
+		return 1;
+	}
+
+	for (;;sleep(1)) {
+		avgs = loadavg();
+		bat = getbattery("/sys/class/power_supply/BAT1");
+        
+        remain_battery = get_battery_dishcharge_time();
+
+		kbmap = execscript("xkblayout-state print '%n' ");
+		soundVolume = execscript("pactl get-sink-volume @DEFAULT_SINK@ | grep -Po '\\d+(?=%)' | head -n 1");
+		t0 = gettemperature("/sys/devices/virtual/thermal/thermal_zone6", "temp");
+		kyivTime = mktimes("%A %d.%m.%Y  %T", timeZoneKyiv);
+		myStatus = smprintf("  %s  󱊣%s%%%s   %s%%   󰌌 %s | %s", t0, bat, remain_battery ? remain_battery : "", soundVolume, kbmap, kyivTime);
+		
+		setstatus(myStatus);
+
+		free(kbmap);
+		free(t0);
+		free(soundVolume);
+		free(avgs);
+		free(bat);
+        free(remain_battery);
+		
+		free(myStatus);
+		free(kyivTime);
+	}
+
+	XCloseDisplay(dpy);
+
+	return 0;
+}
+
